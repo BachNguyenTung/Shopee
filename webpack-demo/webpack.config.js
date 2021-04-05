3c34b833f786658ededa869da7cf6dab17e71edb
tungbach92 <nguyentungbach1992@gmail.com>
2021-04-06 06:56:52 +0700

    generate html webpack

--- webpack-demo/webpack.config.js
+++ webpack-demo/webpack.config.js
@@ -1,9 +1,20 @@
 var path = require("path");
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
   // ...
+  stats: {
+    children: true,
+  },
   mode: "development",
   entry: "./src/index.js",
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: "Output HTML",
+      template: "./src/index.html",
+      inject: "body"
+    })
+  ],
   output: {
     filename: "main.js",
     path: path.resolve(__dirname, "dist"),
